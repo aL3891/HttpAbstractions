3426646537eb94fa1f9b473b7dd932176e53a02a
Mikael Mengistu <mimengis@microsoft.com>
2016-09-23 14:20:03 -0700

    Made equality check methods for each Http method

--- src/Microsoft.AspNetCore.Http.Abstractions/HttpMethods.cs
+++ src/Microsoft.AspNetCore.Http.Abstractions/HttpMethods.cs
@@ -16,9 +16,44 @@ namespace Microsoft.AspNetCore.Http
         public const string Put = "PUT";
         public const string Trace = "TRACE";
 
-        public static bool Equals(string firstMethod, string secondMethod)
+        public static bool IsConnectMethod(string inputMethod)
         {
-            return StringComparer.OrdinalIgnoreCase.Equals(firstMethod, secondMethod);
+            return object.ReferenceEquals(Connect ,inputMethod) | StringComparer.OrdinalIgnoreCase.Equals(Connect, inputMethod);
+        }
+
+        public static bool IsDeleteMethod(string inputMethod)
+        {
+            return object.ReferenceEquals(Delete, inputMethod) | StringComparer.OrdinalIgnoreCase.Equals(Delete, inputMethod);
+        }
+
+        public static bool IsGetMethod(string inputMethod)
+        {
+            return object.ReferenceEquals(Get, inputMethod) | StringComparer.OrdinalIgnoreCase.Equals(Get, inputMethod);
+        }
+
+        public static bool IsOptionsMethod(string inputMethod)
+        {
+            return object.ReferenceEquals(Options, inputMethod) | StringComparer.OrdinalIgnoreCase.Equals(Options, inputMethod);
+        }
+
+        public static bool IsPatchMethod(string inputMethod)
+        {
+            return object.ReferenceEquals(Patch, inputMethod) | StringComparer.OrdinalIgnoreCase.Equals(Patch, inputMethod);
+        }
+
+        public static bool IsPostMethod(string inputMethod)
+        {
+            return object.ReferenceEquals(Post, inputMethod) | StringComparer.OrdinalIgnoreCase.Equals(Post, inputMethod);
+        }
+
+        public static bool IsPutMethod(string inputMethod)
+        {
+            return object.ReferenceEquals(Put, inputMethod) | StringComparer.OrdinalIgnoreCase.Equals(Put, inputMethod);
+        }
+
+        public static bool IsTraceMethod(string inputMethod)
+        {
+            return object.ReferenceEquals(Trace, inputMethod) | StringComparer.OrdinalIgnoreCase.Equals(Trace, inputMethod);
         }
     }
 }
