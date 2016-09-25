3759ad7b1cf90a57cfc924bd3e5829b3e532c045
Mikael Mengistu <ethmengistu7@gmail.com>
2016-09-24 22:17:36 -0700

    Reacting to pr comments

--- src/Microsoft.AspNetCore.Http.Abstractions/HttpMethods.cs
+++ src/Microsoft.AspNetCore.Http.Abstractions/HttpMethods.cs
@@ -17,46 +17,47 @@ namespace Microsoft.AspNetCore.Http
         public const string Put = "PUT";
         public const string Trace = "TRACE";
 
-        public static bool IsConnectMethod(string method)
+        public static bool IsConnect(string method)
         {
             return object.ReferenceEquals(Connect, method) || StringComparer.OrdinalIgnoreCase.Equals(Connect, method);
         }
 
-        public static bool IsDeleteMethod(string method)
+        public static bool IsDelete(string method)
         {
             return object.ReferenceEquals(Delete, method) || StringComparer.OrdinalIgnoreCase.Equals(Delete, method);
         }
 
-        public static bool IsGetMethod(string method)
+        public static bool IsGet(string method)
         {
             return object.ReferenceEquals(Get, method) || StringComparer.OrdinalIgnoreCase.Equals(Get, method);
         }
 
-        public static bool IsHeadMethod(string method){
+        public static bool IsHead(string method)
+        {
             return object.ReferenceEquals(Head, method) || StringComparer.OrdinalIgnoreCase.Equals(Head, method);
         }
 
-        public static bool IsOptionsMethod(string method)
+        public static bool IsOptions(string method)
         {
             return object.ReferenceEquals(Options, method) || StringComparer.OrdinalIgnoreCase.Equals(Options, method);
         }
 
-        public static bool IsPatchMethod(string method)
+        public static bool IsPatch(string method)
         {
             return object.ReferenceEquals(Patch, method) || StringComparer.OrdinalIgnoreCase.Equals(Patch, method);
         }
 
-        public static bool IsPostMethod(string method)
+        public static bool IsPost(string method)
         {
             return object.ReferenceEquals(Post, method) || StringComparer.OrdinalIgnoreCase.Equals(Post, method);
         }
 
-        public static bool IsPutMethod(string method)
+        public static bool IsPut(string method)
         {
             return object.ReferenceEquals(Put, method) || StringComparer.OrdinalIgnoreCase.Equals(Put, method);
         }
 
-        public static bool IsTraceMethod(string method)
+        public static bool IsTrace(string method)
         {
             return object.ReferenceEquals(Trace, method) || StringComparer.OrdinalIgnoreCase.Equals(Trace, method);
         }
