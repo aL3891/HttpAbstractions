c443c510a98ad7a9b1fb47088d044560a604d78f
Mikael Mengistu <ethmengistu7@gmail.com>
2016-09-26 22:52:04 -0700

    Tests now use new class name

--- test/Microsoft.AspNetCore.Owin.Tests/OwinFeatureCollectionTests.cs
+++ test/Microsoft.AspNetCore.Owin.Tests/OwinFeatureCollectionTests.cs
@@ -26,7 +26,7 @@ namespace Microsoft.AspNetCore.Owin
         {
             var env = new Dictionary<string, object>
             {
-                { "owin.RequestMethod", HttpMethods.Post },
+                { "owin.RequestMethod", HttpMethod.Post },
                 { "owin.RequestPath", "/path" },
                 { "owin.RequestPathBase", "/pathBase" },
                 { "owin.RequestQueryString", "name=value" },
@@ -34,7 +34,7 @@ namespace Microsoft.AspNetCore.Owin
             var features = new OwinFeatureCollection(env);
 
             var requestFeature = Get<IHttpRequestFeature>(features);
-            Assert.Equal(requestFeature.Method, HttpMethods.Post);
+            Assert.Equal(requestFeature.Method, HttpMethod.Post);
             Assert.Equal(requestFeature.Path, "/path");
             Assert.Equal(requestFeature.PathBase, "/pathBase");
             Assert.Equal(requestFeature.QueryString, "?name=value");
